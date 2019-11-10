07e18ac42f22130af1a53e160c8d02110decb1b9
andikaandra <andikaandra03@gmail.com>
2019-11-11 01:12:33 +0700

    initial commit

---
+++ apps/config/modules.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'dashboard' => [
+        'namespace' => 'Phalcon\Init\Dashboard',
+        'webControllerNamespace' => 'Phalcon\Init\Dashboard\Controllers\Web',
+        'apiControllerNamespace' => 'Phalcon\Init\Dashboard\Controllers\Api',
+        'className' => 'Phalcon\Init\Dashboard\Module',
+        'path' => APP_PATH . '/modules/dashboard/Module.php',
+        'defaultRouting' => true,
+        'defaultController' => 'dashboard',
+        'defaultAction' => 'index'
+    ],
+
+);
